00b285a693384e3063bd77282ec379758d0756aa
Danik Collin <2655393@collegelacite.ca>
2023-04-08 13:05:00 -0400

    Finished starscanvas

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { Suspense } from "react";
+
+import { Canvas} from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF} from "@react-three/drei";
+import Loader from "../Loader";
 
 const Earth = () => {
+  const earth = useGLTF("/planet/scene.gltf", true);
   return (
-    <div>Earth</div>
+    <primitive object={earth.scene} scale={2.5} position-y={0} position-x={0} >
+      
+    </primitive>
   )
 }
 
-export default Earth
+const EarthCanvas = () => {
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      camera={{ fov: 45, near: 0.1, far: 200, position: [-4, 3, 6] }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<Loader />}>
+        <OrbitControls
+          autoRotate
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Earth />
+      </Suspense>
+    </Canvas>
+  )
+  }
+export default EarthCanvas;
--- src/constants/index.js
+++ src/constants/index.js
@@ -54,7 +54,7 @@ import {
       icon: backend,
     },
     {
-      title: "Content Creator",
+      title: "Database Engineer",
       icon: creator,
     },
   ];
