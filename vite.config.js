43d8d0d48449bdcd65a041b6333b67ce3a58d46b
Danik Collin <2655393@collegelacite.ca>
2023-04-24 12:49:23 -0400

    changed vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default {
-  plugins: [react({jsxRuntime: 'automatic'})],
-};
+export default defineConfig({
+  plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? '/3d_portfolio/' : '/',
+});
